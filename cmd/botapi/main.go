4ec190975732fedf08792347833e9d8b301f4de9
b2r2 <b2r2@bk.ru>
2019-11-15 21:26:29 +0300

    added new file

---
+++ cmd/botapi/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/ChatControl/internal/app"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "./configs/config.toml", "path to config file")
+}
+
+func main() {
+	flag.Parse()
+	config := app.NewConfig()
+	if _, err := toml.DecodeFile(configPath, config); err != nil {
+		log.Fatal(err)
+	}
+	b := app.NewBotAPI(config)
+	if err := b.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
