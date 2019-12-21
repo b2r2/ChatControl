9e57675b3226101c92dd9517199b218b15948921
b2r2 <b2r2@bk.ru>
2019-12-21 14:01:01 +0300

    refactoring

--- internal/app/config.go
+++ internal/app/config.go
@@ -5,7 +5,6 @@ type Config struct {
 	Token          string   `toml:"token"`
 	DebugMode      bool     `toml:"debug_mode"`
 	LogLevel       string   `toml:"log_level"`
-	Regexp         string   `toml:"regexp"`
 	AccessUsers    []string `toml:"access_users"`
 	AccessChannels []string `toml:"access_channels"`
 }
